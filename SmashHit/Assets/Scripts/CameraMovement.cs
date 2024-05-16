e96fb34239a3a9b76ed22916f040165f0ee4624c
Musashi-Harunobu <karen20050105@gmail.com>
2024-05-17 03:38:25 +0400

    throwing

--- SmashHit/Assets/Scripts/CameraMovement.cs
+++ SmashHit/Assets/Scripts/CameraMovement.cs
@@ -2,14 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private float cameraSpeed = 10;
 
-
+    public UnityEvent OnMoveBreak;
+    
+    private void Awake()
+    {
+        GameManager.OnLoss.AddListener(BreakMovement);
+    }
     private void FixedUpdate()
     {
         transform.position += Vector3.forward * cameraSpeed * Time.fixedDeltaTime;
+    }
+    public void BreakMovement()
+    {
+        cameraSpeed = 0;
+        OnMoveBreak.Invoke();
     }
 }
